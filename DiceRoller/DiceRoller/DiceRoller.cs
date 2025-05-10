c7172d1245d3b6b3c28dac164b4694e453f8e72f
roberto <robertsouaz@hotmail.com>
2025-05-09 21:50:15 -0300

    at

---
+++ DiceRoller/DiceRoller/DiceRoller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    internal class DiceRoller
+    {
+        private int ladoSorteado;
+
+        public int LadoSorteado { get => ladoSorteado; set => ladoSorteado = value; }
+
+        public DiceRoller()
+        {
+            LadoSorteado = 1;
+        }
+
+        public int Flip()
+        {
+            Random random = new Random();
+            int resultado = random.Next(1,7);
+            return (LadoSorteado);
+        }
+    }
+}
+
---
+++ DiceRoller/DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void FlipButton_Clicked(object sender, EventArgs e)
+        {
+
+            DiceRoller dice = new DiceRoller();
+            int sorteio = dice.Flip();
+
+            DadoImage1.Source = null;
+            DadoImage1.Source = $"dado{sorteio}.png";
+
+            if (SelecaoPicker.SelectedIndex + 1 == sorteio)
+            {
+                DisplayAlert("Parabéns", "Você venceu!", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Se ferrou", "Você perdeu!", "Ok");
+            }
+        }
+    }
+}
